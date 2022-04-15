9bff3cb9138b10abcec16754e3206f7cefb9832a
JMr <jramallo@ombu.net>
2022-04-15 11:13:50 -0300

    Up api Start Seting enviorment

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+* Primary file for the API
+*
+*/
+
+// Dependencies
+var http = require('http');
+
+// The server should responde to all request with a string
+var server=http.createServer((req, res)=>{
+    res.end('Hello World\n')
+});
+
+
+// The start server, and have listen to port 3000
+server.listen(3000,function(){
+    console.log("The server is listening on port 3000 now")
+})
